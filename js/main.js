e92f7827e26cda035a465b937632b1b1e8c2cd39
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-09-30 12:05:23 +0500

    Upload all images

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ const elLoader = document.querySelector(".js-loader");
 document.addEventListener("DOMContentLoaded", function (){
   setTimeout(function ()  {
     elLoader.classList.add("lds-ellipsis-wrapper--none");
-  }, 800);
+  }, 1000);
 });
