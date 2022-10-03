0ac31e2086d94da8fbb1a05073186500cd19ae54
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-10-03 12:21:44 +0500

    Add cta fully

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-const elLoader = document.querySelector(".js-loader");
+const elLoader = document.querySelector(".js-loader"),
+elInput = document.querySelector(".js-input"),
+elButton = document.querySelector(".js-img-button"),
+elLink = document.querySelector(".js-img-link");
 
 document.addEventListener("DOMContentLoaded", function (){
   setTimeout(function ()  {
     elLoader.classList.add("lds-ellipsis-wrapper--none");
   }, 800);
 });
+
+// if (elInput) {
+//   elInput.addEventListener("input", function () {
+//     elLink.classList.add("form__link-img--none");
+//     elButton.classList.add("form__button--one");
+//   });
+// } else {
+//   elLink.classList.remove("form__link-img--none");
+//   elButton.classList.remove("form__button--one");
+// }
